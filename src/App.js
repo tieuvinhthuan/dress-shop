51418c57e2996b9e019548837e746a9d105ab407
unknown <thuan@home.com>
2020-04-12 07:25:34 +0300

    Create and format a right side navigation

--- src/App.js
+++ src/App.js
@@ -3,20 +3,23 @@ import Navbar from './components/Navbar'
 import { Route, BrowserRouter } from 'react-router-dom'
 import Home from './components/Home'
 import About from './components/About'
-
-
-
+import M from  'materialize-css/dist/js/materialize.min.js';
 
 
 class App extends Component {
+  componentDidMount() {
+    let sidenav = document.querySelector('#mobile');
+    M.Sidenav.init(sidenav, {edge:'right'});
+  }
+
   render() {
     return (
       <BrowserRouter>
         <div className="App">
           <Navbar />
 
-            <Route exact path='/' component={Home}/>
-            <Route path='/about' component={About} />
+          <Route exact path='/' component={Home}/>
+          <Route path='/about' component={About} />
             
           
         </div>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,19 +1,29 @@
 import React from 'react';
 import { Link, NavLink, withRouter } from 'react-router-dom';
+import 'materialize-css/dist/css/materialize.min.css';
 
 const Navbar = (props) => {
   return (
+    
     <nav className="nav-wrapper light-green darken-2">
       <div className="container">
         <Link className="brand-logo" to="/">Oh-la-Le Shop</Link>
-        <ul className="right">
+        <a href="#" data-target="mobile" class="sidenav-trigger right"><i class="material-icons">menu</i></a>
+        <ul className="right hide-on-med-and-down">
           <li><NavLink exact to="/">Main Page</NavLink></li>
           <li><NavLink to='/about'>About</NavLink></li>
          
         </ul>
       </div>
+      <ul className="sidenav" id="mobile">
+        <li><NavLink exact to="/">Main Page</NavLink></li>
+        <li><NavLink to='/about'>About</NavLink></li>
+      </ul>
     </nav>
-  )
+    
+    
+    
+        )
 }
 
 export default withRouter(Navbar);
