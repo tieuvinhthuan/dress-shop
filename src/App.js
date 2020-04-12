c5084089d2f227082967f6300e7fc7c2c3a2fd30
unknown <thuan@home.com>
2020-04-12 04:12:49 +0300

    update image hover and zoom

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Navbar from './components/Navbar'
 import { Route, BrowserRouter } from 'react-router-dom'
 import Home from './components/Home'
 import About from './components/About'
-import Contact from './components/Contact'
+
 
 
 
@@ -17,7 +17,7 @@ class App extends Component {
 
             <Route exact path='/' component={Home}/>
             <Route path='/about' component={About} />
-            <Route path='/contact' component={Contact} />
+            
           
         </div>
       </BrowserRouter>
--- src/components/Products.js
+++ src/components/Products.js
@@ -11,15 +11,16 @@ export default class Products extends Component {
         const productItems = this.props.products.map(product => (
             <div className="col-md-3" key={product.id}>
                 <div className="thumbnail text-center">
-                        <img src={`products/${product.sku}_2.jpg`} alt={product.title} />
-                        
-                        <p>{product.title} <br/>      
-                            <a href={`products/${product.sku}_1.jpg` } target="_blank" rel="noopener noreferrer">
-                                See this product
-                            </a> <br/>
-                            <b>{util.formatCurrency(product.price)}</b> <br/>                                     
-                            <button className="btn btn-success" onClick={(e)=>this.props.handleAddToCart(e, product)}>Add to cart</button>
-                        </p>
+                
+                    <img id="zoom" src={`products/${product.p_code}_1.jpg`} alt={product.title} />
+                
+                    <p>{product.title} <br/>      
+                        <a href={`products/${product.p_code}_1.jpg` } target="_blank" rel="noopener noreferrer">
+                            See this product
+                        </a> <br/>
+                        <b>{util.formatCurrency(product.price)}</b> <br/>                                     
+                        <button className="btn btn-success" onClick={(e)=>this.props.handleAddToCart(e, product)}>Add to cart</button>
+                    </p>
                           
                 </div>
             </div>
@@ -30,5 +31,7 @@ export default class Products extends Component {
                 {productItems}
             </div>
         )
+
+        
     }
 }
